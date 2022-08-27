13bf65af0acb9b0ff8e58bbcba0ea2488ac1a6b3
Afnan312 <109278329+Afnan312@users.noreply.github.com>
2022-08-27 12:15:49 +0600

    Add files via upload

---
+++ SQLdumps/new/additional procedures.sql
@@ -0,0 +1,30 @@
+--edit review
+
+CREATE OR REPLACE procedure editReview(pID in NUMBER, uID in number, comment in VARCHAR2, uRating in number) IS
+	rID NUMBER;
+BEGIN
+	select REVIEWID into rID from PROPERTYREVIEW where PROPERTYID=pID and GUESTID=uID;
+	UPDATE PROPERTYREVIEW set COMMENTFORPROPERTY=comment, RATING=uRating where REVIEWID=rID;
+end;
+/
+
+--edit ta review
+
+CREATE OR REPLACE procedure editTAReview(ntaID in NUMBER, uID in number, comment in VARCHAR2, uRating in number) IS
+	rID number;
+BEGIN
+	select REVIEWID into rID from TAREVIEW where TAID=ntaID and USERID=uID;
+	UPDATE TAREVIEW set COMMENTFORTA=comment, RATING=uRating where REVIEWID=rID;
+end;
+/
+
+--remove user
+
+CREATE OR REPLACE procedure removeUser(uID IN NUMBER) IS
+begin
+	update AIRBNBUSER set status='block' where USERID=uID;
+	DELETE from PROPERTY where HOSTID=uID;
+	delete from GUEST where GUESTID=uID;
+	delete from HOST where HOSTID=uID;
+END;
+/
